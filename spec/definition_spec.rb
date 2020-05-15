c89f2c280cee04e77e8dc1ec426d976ed75fa6e5
austinb847 <austinb847@icloud.com>
2020-05-15 11:55:15 -0700

    meaningful fail test for save method

---
+++ spec/definition_spec.rb
@@ -0,0 +1,23 @@
+require 'rspec'
+require 'word'
+require 'definition'
+require 'pry'
+
+describe('#Definition') do
+  before(:each) do
+    Definition.clear()
+    Word.clear()
+    @word = Word.new({:name => "Nutrition", :id => nil})
+    @word.save()
+  end
+
+  describe('#save') do
+    it('saves a definition') do
+      definition = Definition.new({:word_definition => "this is a definition of a word", :id => nil, :word_id => @word_id})
+      definition.save()
+      definition2 = Definition.new({:word_definition => "this is another definition of a word", :id => nil, :word_id => @word_id})
+      definition2.save()
+      expect(Definition.all).to(eq([definition, definition2]))
+    end
+  end
+end
