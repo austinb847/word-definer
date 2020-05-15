781a1b71de9a78711c6f6f8b6b27c582dd0f26b4
austinb847 <austinb847@icloud.com>
2020-05-15 10:34:04 -0700

    add save method, all method, and comparison method

--- lib/word.rb
+++ lib/word.rb
@@ -10,5 +10,17 @@ class Word
     @name = attributes.fetch(:name)
     @id = attributes.fetch(:id) || @@total_rows += 1
   end
+
+  def save()
+    @@words[self.id] = Word.new({:name => self.name, :id => self.id})
+  end
+
+  def self.all()
+    @@words.values
+  end
+
+  def ==(word_to_compare)
+    self.name() == word_to_compare.name()
+  end
   
 end
