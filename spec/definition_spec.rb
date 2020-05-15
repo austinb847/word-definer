016720f2c9994e573db88f8b06e5ecb6714206c2
austinb847 <austinb847@icloud.com>
2020-05-15 14:02:35 -0700

    meaningful fail test for find_by_word method

--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -57,4 +57,14 @@ describe('#Definition') do
       expect(Definition.find(definition2.id)).to(eq(definition2))
     end
   end
+
+  describe('.find_by_word') do
+    it('finds all definitions for a Word based on word_id') do
+      definition = Definition.new({:word_definition => "this is a definition of a word", :id => nil, :word_id => @word_id})
+      definition.save()
+      definition2 = Definition.new({:word_definition => "this is another definition of a word", :id => nil, :word_id => @word_id})
+      definition2.save()
+      expect(Definition.find_by_word(@word.id)).to(eq([definition,definition2]))
+    end
+  end
 end
