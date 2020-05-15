3a2d36218b87a1d7713623a07e98d24bf28c0186
austinb847 <austinb847@icloud.com>
2020-05-15 12:08:31 -0700

    meaningful fail test for update method

--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -26,4 +26,14 @@ describe('#Definition') do
       expect(Definition.all).to(eq([]))
     end
   end
+
+  describe('#update') do
+    it('updates a definition with a new definition') do
+      definition = Definition.new({:word_definition => "this is a definition of a word", :id => nil, :word_id => @word_id})
+      definition.save()
+      definition.update("this is an updated definition")
+      expect(definition.word_definition).to(eq("this is an updated definition"))
+
+    end
+  end
 end
